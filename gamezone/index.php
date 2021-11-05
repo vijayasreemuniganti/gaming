10f6079e9e40e88f2b8f79428b810149d20a53fb
vijayasreemuniganti <79300082+vijayasreemuniganti@users.noreply.github.com>
2021-11-05 17:40:28 +0530

    Add files via upload

---
+++ gamezone/index.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-md-8 col-lg-8 text-center mx-auto">
+                <h2 class="text-center border rounded p-2">Welcome To Gueesing Game</h2>
+                <p><i>You will have <b>5</b> tries to guess the correct number <b>(1-20)</b></i></p>
+                <h4 class="text-success "><i>GOOD LUCK</i></h4>
+
+                <a href="guessingGame.php" class="btn btn-lg btn-danger text-center">Start The Game</a>
+
+            </div>
+        </div>
+    </div>
+
+
+    <?php
+    $_SESSION['rand'] = rand(1, 20);
+    //echo $_SESSION['rand'];
+    $_SESSION['counter'] = 0;
+    ?>
+    <div style="height: 300px;">
+
+    </div>
+
+    
+
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+
+</html>
